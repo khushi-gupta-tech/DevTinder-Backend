bfe56108bd846252e76e3381f679b8aeb9cbdadb
khushi gupta <kg834208@gmail.com>
2025-11-04 00:41:42 +0530

    playing with the routes

--- src/app.js
+++ src/app.js
@@ -2,27 +2,11 @@ const express = require('express');
 
 const app = express();
 
-app.use("/user",(req,res)=>{
-   res.send("HAJHAHAJA")
-})
-// this will only handle GET call to /user
-app.get("/user",(req,res)=>{
+app.get("/user/:userId/:name/:password",(req,res)=>{
+    console.log(req.params)
+  //  console.log(req.query);
     res.send({firstname:"khushi",lastname:"gupta"})
 })
-
-app.post("/user",(req,res)=>{
-   console.log("save data")
-   res.send("Data Saved successfully")
-})
-
-app.delete("/user",(req,res)=>{
-    res.send("Deleted successfully")
-})
-// this will match all the http method API calls to /test
-app.use("/test",(req,res)=>{
- res.send("hello Server")
-})
-
 
 
 app.listen(3000,()=>{
