8afdbe9a8013032e8954b7e4d3f69ceec1cc2c3d
khushi gupta <kg834208@gmail.com>
2025-11-04 00:20:35 +0530

    explore http methods

--- src/app.js
+++ src/app.js
@@ -2,9 +2,28 @@ const express = require('express');
 
 const app = express();
 
+app.use("/user",(req,res)=>{
+   res.send("HAJHAHAJA")
+})
+// this will only handle GET call to /user
+app.get("/user",(req,res)=>{
+    res.send({firstname:"khushi",lastname:"gupta"})
+})
+
+app.post("/user",(req,res)=>{
+   console.log("save data")
+   res.send("Data Saved successfully")
+})
+
+app.delete("/user",(req,res)=>{
+    res.send("Deleted successfully")
+})
+// this will match all the http method API calls to /test
 app.use("/test",(req,res)=>{
  res.send("hello Server")
 })
+
+
 
 app.listen(3000,()=>{
     console.log("Server is successfully listening")
