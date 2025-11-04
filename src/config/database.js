cfb33aa56d561d5311c5ea326edc69fd243519ec
khushi gupta <kg834208@gmail.com>
2025-11-04 22:25:34 +0530

    secure the key

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  await mongoose.connect(
-    "mongodb+srv://kg834208_db_user:6NZYEtmIOR631acg@devtindercluster.tcqqzyp.mongodb.net/devTinder"
-  );
+  await mongoose.connect(process.env.MONGODB_URL);
 };
 
-module.exports = {connectDB};
- 
+module.exports = { connectDB };
