dadea129c0583c277b2007b6a2b3664e36e04c94
khushi gupta <kg834208@gmail.com>
2025-11-05 00:53:29 +0530

    patch and delete api

--- src/app.js
+++ src/app.js
@@ -22,28 +22,50 @@ app.get("/user", async (req, res) => {
   const userEmail = req.body.emailId;
   try {
     const users = await User.findOne({ emailId: userEmail });
-    if(users.length===0){
-       res.status(404).send("User not found")
-    }else{
-     res.send(users);
+    if (users.length === 0) {
+      res.status(404).send("User not found");
+    } else {
+      res.send(users);
     }
-    
   } catch (err) {
     res.status(400).send("Something went wrong");
   }
 });
 // Feed API - get all users from the database
-app.get("/feed", async(req, res) => {
-    try{
-      const users = await User.find({})
-      res.send(users)
-    }
-    catch(err){
-      res.status(400).send("Not Found")
-    }
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
+    res.status(400).send("Not Found");
+  }
 });
 
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
 
+  try {
+    const user = await User.findByIdAndDelete(userId);
+    res.send("user deleted successfully");
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+// update data from the database
+
+app.patch("/user",async(req,res)=>{
+    const userId = req.body.userId;
+    const data = req.body;
+
+    try{
+      const user = await User.findByIdAndUpdate({_id:userId},data,{returnDocument:"before"})
+      res.send("updated successfully")
+    }catch(err){
+     res.status(400).send('Something went wrong')
+    }
+
+})
 connectDB()
   .then(() => {
     console.log("Database connection established...");
