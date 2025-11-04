309dfa5cf20ece6546669448d303d579a4bd5266
khushi gupta <kg834208@gmail.com>
2025-11-04 21:19:39 +0530

    handle error

--- src/app.js
+++ src/app.js
@@ -14,6 +14,11 @@ app.get("/admin/getAllData",(req,res)=>{
     res.send("All Data send")
 })
 
+app.use("/",(err,req,res,next)=>{
+    if(err){
+        res.status(500).send("something went wrong")
+    }
+})
 app.listen(3000,()=>{
     console.log("Server is successfully listening")
 });
