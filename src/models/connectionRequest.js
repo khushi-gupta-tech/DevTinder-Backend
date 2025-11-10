a962e56e44b64292b7987c5d8f8443841c330f1f
khushi gupta <kg834208@gmail.com>
2025-11-10 23:48:20 +0530

    create request/send api

--- src/models/connectionRequest.js
+++ src/models/connectionRequest.js
@@ -23,6 +23,14 @@ const connectionRequestSchema = new mongoose.Schema(
   }
 );
 
+connectionRequestSchema.pre("save", function (next) {
+  const connectionRequest = this;
+  // Check if the fromUserId is same as toUserId
+  if (connectionRequest.fromUserId.equals(connectionRequest.toUserId)) {
+    throw new Error("Cannot send connection request to yourself!");
+  }
+  next();
+});
 const ConnectionRequestModel = new mongoose.model(
   "ConnectionRequest",
   connectionRequestSchema
