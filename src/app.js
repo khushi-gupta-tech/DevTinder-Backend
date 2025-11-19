e3322fbe0be6c11dea4312a2f8a667bd5765da91
khushi gupta <kg834208@gmail.com>
2025-11-20 00:00:46 +0530

    handle cors issue

--- src/app.js
+++ src/app.js
@@ -2,9 +2,14 @@ require("dotenv").config();
 const express = require("express");
 const { connectDB } = require("./config/database");
 const app = express();
+const cors = require("cors");
 
 const cookieParser = require("cookie-parser");
 
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true
+}));
 app.use(express.json());
 app.use(cookieParser());
 
