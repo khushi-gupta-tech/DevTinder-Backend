f12ec3db13b64a667f69a6abe4c9c67ec1afffba
khushi gupta <kg834208@gmail.com>
2025-11-30 20:19:30 +0530

    create new model and write api

--- src/routes/payment.js
+++ src/routes/payment.js
@@ -1,11 +1,44 @@
 const express = require("express");
 const { userAuth } = require("../middlewares/auth");
 const paymentRouter = express.Router();
+const razorpayInstance = require("../utils/razorpay");
+const Payment = require("../models/paymentOrder");
+const membershipAmount = require("../utils/constants");
 
 paymentRouter.post("/payment/create", userAuth, async (req, res) => {
   try {
-    
-  } catch (err) {}
+     const {membershipType} = req.body;
+     const {firstName,lastName,emailId} = req.user;
+
+    const order = await razorpayInstance.orders.create({
+      amount: membershipAmount[membershipType] * 100,
+      currency: "INR",
+      receipt: "receipt_" + Date.now(),
+      notes: {
+        firstName,
+        lastName,
+        emailId,
+        membershipType:membershipType,
+      },
+    });
+
+    // save it in my database
+    const payment = new Payment({
+      userId: req.user._id,
+      orderId: order.id,
+      status: order.status,
+      amount: order.amount,
+      currency: order.currency,
+      reciept: order.receipt,
+      notes: order.notes,
+    });
+    const savedPayment = await payment.save();
+    res.json({ ...savedPayment.toJSON() });
+  } catch (err) {
+    res.status(500).json({
+      error: err.error ? err.error.description : err.message,
+    });
+  }
 });
 
 module.exports = paymentRouter;
--- src/utils/razorpay.js
+++ src/utils/razorpay.js
@@ -2,7 +2,7 @@ const Razorpay = require("razorpay");
 
 var instance = new Razorpay({
   key_id: process.env.RAZORPAY_KEY_ID,
-  key_secret: process.env.Y_KEY_SECRET,
+  key_secret: process.env.RAZORPAY_KEY_SECRET,
 });
 
-module.exports = instance
+module.exports = instance;
