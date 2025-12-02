5fb597487578de6445e84366f7d52aa426a43379
khushi gupta <kg834208@gmail.com>
2025-12-02 21:41:11 +0530

    done chat connection

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,14 +1,36 @@
 const socket = require("socket.io");
+const crypto = require("crypto");
+const { ro } = require("date-fns/locale");
+
+const getSecretRoomId = (userId, targetUserId) => {
+  return crypto
+    .createHash("sha256")
+    .update([userId, targetUserId].sort().join("_"))
+    .digest("hex");
+};
 
 const initializeSocket = (server) => {
   const io = socket(server, {
     cors: {
-      origin: "https://localhost:5173",
+      origin: "http://localhost:5173",
     },
   });
 
   io.on("connection", (socket) => {
     // handle events
+
+    socket.on("joinChat", ({ userId, targetUserId }) => {
+      const roomId = getSecretRoomId(userId, targetUserId);
+      socket.join(roomId);
+    });
+
+    socket.on("sendMessage", ({ firstName, userId, targetUserId, text }) => {
+      const roomId = getSecretRoomId(userId, targetUserId);
+      
+      io.to(roomId).emit("messageReceived", { firstName, text });
+    });
+
+    socket.on("disconnect", () => {});
   });
 };
- module.exports = initializeSocket;
+module.exports = initializeSocket;
